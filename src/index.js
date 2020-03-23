91fe7641e6f731cd10df86d96398d35f5cb81077
Behemoth86 <megadeth86@mail.ru>
2020-03-23 11:29:43 +0400

    feat: add solution for brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let Arr = [];
+	for (let i = 0; i<bracketsConfig.length; i++) {Arr[i] = bracketsConfig[i].join('');}
+	for (let i = 0; i<Arr.length; i++) {
+		while(str.includes(Arr[i])) {
+			str=str.replace(Arr[i],'');
+			i=0;
+		}
+	}
+	return str.length===0?true:false;
 }
